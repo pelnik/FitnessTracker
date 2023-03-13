63ed37777dd60b16180a9d66a662fc3daeb5d465
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-13 10:20:41 -0500

    add more get user functions

--- db/users.js
+++ db/users.js
@@ -34,17 +34,67 @@ async function createUser({ username, password }) {
 }
 
 // this function should return a single user (object) from the database that matches the userName that is passed in as an argument.
-async function getUserByUsernameWithPassword(userName) {}
+async function getUserByUsernameWithPassword(userName) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [userName]
+    );
 
-// this should be able to verify the password against the hashed password and if the passwords match then it should return a single user (object) from the database that matches the username that is passed in as part of the argument
-async function getUser({ username, password }) {
-  const user = await getUserByUsername(username);
-  const hashedPassword = user.password;
-  const isValid = await bcrypt.compare(password, hashedPassword);
+    return user;
+  } catch (error) {
+    console.error('error getting user by username');
+    throw error;
+  }
 }
 
-// this function should return a single user (object) from the database that matches the id that is passed in as an argument.
-async function getUserById(userId) {}
+// this should be able to verify the password against the hashed password
+// and if the passwords match then it
+// should return a single user (object) from the database that matches the
+// username that is passed in as part of the argument
+async function getUser({ username, password }) {
+  try {
+    const user = await getUserByUsernameWithPassword(username);
+    const hashedPassword = user.password;
+    const isValid = await bcrypt.compare(password, hashedPassword);
+
+    delete user.password;
+
+    if (isValid) {
+      return user;
+    }
+
+    return null;
+  } catch (error) {
+    console.error('error getting user');
+    throw error;
+  }
+}
+
+// this function should return a single user (object) from the database
+// that matches the id that is passed in as an argument.
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+      SELECT id, username
+      FROM users
+      WHERE id = ${userId};
+    `);
+
+    return user;
+  } catch (error) {
+    console.error(`error getting user by id`);
+    throw error;
+  }
+}
 
 module.exports = {
   createUser,
