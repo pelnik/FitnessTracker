f8ccc9d094fa83e34e8de33748c9e1e07f46eda3
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-16 10:27:31 -0500

    add fly io files

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const http = require("http")
-const app = require("./app")
+const http = require('http');
+const app = require('./app');
 
-const { PORT = 3000 } = process.env;
-const server = http.createServer(app)
+const port = process.env.PORT || '8080';
+const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log("The server is up on port", PORT);
+server.listen(port, () => {
+  console.log('The server is up on port', port);
 });
-
-
